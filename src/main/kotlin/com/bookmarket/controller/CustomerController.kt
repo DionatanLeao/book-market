738b599df04e02134c46d74a0c5b1b9874fd46bf
DionatanLeao <dionatan.ti@outlook.com>
2023-08-23 23:07:21 -0300

    feature/Lesson-01: Creating our first endpoint

---
+++ src/main/kotlin/com/bookmarket/controller/CustomerController.kt
@@ -0,0 +1,13 @@
+package com.bookmarket.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping
+class CustomerController {
+
+    @GetMapping
+    fun helloWorld() = "Hello World"
+}
