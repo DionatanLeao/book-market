8b899f926549148ea90e18a665163a0829a85da4
DionatanLeao <dionatan.ti@outlook.com>
2023-09-03 06:01:12 -0300

    feature/Lesson-05: Changing the Return Status

--- src/main/kotlin/com/bookmarket/controller/CustomerController.kt
+++ src/main/kotlin/com/bookmarket/controller/CustomerController.kt
@@ -2,10 +2,12 @@ package com.bookmarket.controller
 
 import com.bookmarket.controller.request.PostCustomerRequest
 import com.bookmarket.model.CustomerModel
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -13,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController
 class CustomerController {
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     fun getCustomer() = CustomerModel("1", "Customer1", "customer1@email.com")
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer: PostCustomerRequest) {
         println("create ::: $customer")
     }
