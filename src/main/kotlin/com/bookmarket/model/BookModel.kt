f002977389c0afb6c75da6e6553082ead6095022
DionatanLeao <dionatan.ti@outlook.com>
2024-01-06 19:37:44 -0300

    feature/Lesson-24: Blocking status change of cancelled/deleted books

--- src/main/kotlin/com/bookmarket/model/BookModel.kt
+++ src/main/kotlin/com/bookmarket/model/BookModel.kt
@@ -16,11 +16,28 @@ data class BookModel(
     @Column
     var price: BigDecimal,
 
-    @Column
-    @Enumerated(EnumType.STRING)
-    var status: BookStatus? = null,
-
     @ManyToOne
     @JoinColumn(name = "customer_id")
     var customer: CustomerModel? = null
-)
+) {
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    var status: BookStatus? = null
+        set(value) {
+            if (field == BookStatus.CANCELLED || field == BookStatus.DELETED) {
+                throw Exception("Cannot change a book with the status $field")
+            }
+            field = value
+        }
+
+    constructor(
+        id: Int? = null,
+        name: String,
+        price: BigDecimal,
+        customer: CustomerModel? = null,
+        status: BookStatus?
+    ): this(id, name, price, customer) {
+        this.status = status
+    }
+}
