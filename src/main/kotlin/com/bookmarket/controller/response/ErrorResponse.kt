9ade2a7e3a9c0d21be994a9f65a87442ed61b8df
DionatanLeao <dionatan.ti@outlook.com>
2024-01-13 17:15:39 -0300

    feature/Lesson-27: How should we return the errors

---
+++ src/main/kotlin/com/bookmarket/controller/response/ErrorResponse.kt
@@ -0,0 +1,8 @@
+package com.bookmarket.controller.response
+
+data class ErrorResponse(
+    var httpCode: Int,
+    var message: String,
+    var internalCode: String,
+    var errors: List<FieldErrorResponse>?
+)
