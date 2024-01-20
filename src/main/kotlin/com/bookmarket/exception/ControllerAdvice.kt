9b9799cb0df672bbd548d543c08fddf0ee31c81b
DionatanLeao <dionatan.ti@outlook.com>
2024-01-20 10:22:29 -0300

    feature/Lesson-31: Creating the Bad Request Exception

--- src/main/kotlin/com/bookmarket/exception/ControllerAdvice.kt
+++ src/main/kotlin/com/bookmarket/exception/ControllerAdvice.kt
@@ -11,9 +11,20 @@ import org.springframework.web.context.request.WebRequest
 class ControllerAdvice {
 
     @ExceptionHandler(NotFoundException::class)
-    fun handleException(ex: NotFoundException, request: WebRequest): ResponseEntity<ErrorResponse> {
+    fun handleNotFoundException(ex: NotFoundException, request: WebRequest): ResponseEntity<ErrorResponse> {
         val error = ErrorResponse(
             HttpStatus.NOT_FOUND.value(),
+            ex.message,
+            ex.errorCode,
+            null
+        )
+        return ResponseEntity(error, HttpStatus.NOT_FOUND)
+    }
+
+    @ExceptionHandler(BadRequestException::class)
+    fun handleBadRequestException(ex: BadRequestException, request: WebRequest): ResponseEntity<ErrorResponse> {
+        val error = ErrorResponse(
+            HttpStatus.BAD_REQUEST.value(),
             ex.message,
             ex.errorCode,
             null
