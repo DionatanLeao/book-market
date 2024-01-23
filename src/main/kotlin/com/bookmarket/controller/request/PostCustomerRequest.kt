48c93aac282207b3d43a2c32eb4aa2583d0c47e3
DionatanLeao <dionatan.ti@outlook.com>
2024-01-23 12:48:34 -0300

    feature/Lesson-32: Request Validations with Spring Validator

--- src/main/kotlin/com/bookmarket/controller/request/PostCustomerRequest.kt
+++ src/main/kotlin/com/bookmarket/controller/request/PostCustomerRequest.kt
@@ -1,6 +1,11 @@
 package com.bookmarket.controller.request
 
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotEmpty
+
 data class PostCustomerRequest(
+    @field:NotEmpty
     val name: String,
+    @field:Email
     val email: String
 )
