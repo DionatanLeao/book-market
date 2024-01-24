4ef011a1320dbd1c7425cc2cccc3ce52b5fdd358
DionatanLeao <dionatan.ti@outlook.com>
2024-01-23 21:49:40 -0300

    feature/Lesson-33: Handling the thrown exception

--- src/main/kotlin/com/bookmarket/enuns/Errors.kt
+++ src/main/kotlin/com/bookmarket/enuns/Errors.kt
@@ -5,6 +5,7 @@ enum class Errors(
     val message: String
 ) {
 
+    ML001("ML-001", "Invalid Request"),
     ML101("ML-101", "Book [%s] not exists"),
     ML102("ML-102", "Cannot change a book with the status [%s]"),
     ML201("ML-201", "Customer [%s] not exists")
--- src/main/kotlin/com/bookmarket/exception/ControllerAdvice.kt
+++ src/main/kotlin/com/bookmarket/exception/ControllerAdvice.kt
@@ -1,8 +1,11 @@
 package com.bookmarket.exception
 
 import com.bookmarket.controller.response.ErrorResponse
+import com.bookmarket.controller.response.FieldErrorResponse
+import com.bookmarket.enuns.Errors
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.WebRequest
@@ -11,7 +14,10 @@ import org.springframework.web.context.request.WebRequest
 class ControllerAdvice {
 
     @ExceptionHandler(NotFoundException::class)
-    fun handleNotFoundException(ex: NotFoundException, request: WebRequest): ResponseEntity<ErrorResponse> {
+    fun handleNotFoundException(
+        ex: NotFoundException,
+        request: WebRequest
+    ): ResponseEntity<ErrorResponse> {
         val error = ErrorResponse(
             HttpStatus.NOT_FOUND.value(),
             ex.message,
@@ -22,12 +28,29 @@ class ControllerAdvice {
     }
 
     @ExceptionHandler(BadRequestException::class)
-    fun handleBadRequestException(ex: BadRequestException, request: WebRequest): ResponseEntity<ErrorResponse> {
+    fun handleBadRequestException(
+        ex: BadRequestException,
+        request: WebRequest
+    ): ResponseEntity<ErrorResponse> {
         val error = ErrorResponse(
             HttpStatus.BAD_REQUEST.value(),
             ex.message,
             ex.errorCode,
             null
+        )
+        return ResponseEntity(error, HttpStatus.NOT_FOUND)
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArgumentNotValidException(
+        ex: MethodArgumentNotValidException,
+        request: WebRequest
+    ): ResponseEntity<ErrorResponse> {
+        val error = ErrorResponse(
+            HttpStatus.UNPROCESSABLE_ENTITY.value(),
+            Errors.ML001.message,
+            Errors.ML001.code,
+            ex.bindingResult.fieldErrors.map { FieldErrorResponse(it.defaultMessage ?: "Invalid", it.field) }
         )
         return ResponseEntity(error, HttpStatus.NOT_FOUND)
     }
