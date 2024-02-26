50f32ef146e9b0c9496613aee0169cfb3600b469
DionatanLeao <dionatan.ti@outlook.com>
2024-02-26 14:01:27 -0300

    feature/Lesson-40: Firing up our first Event

--- src/main/kotlin/com/bookmarket/controller/PurchaseController.kt
+++ src/main/kotlin/com/bookmarket/controller/PurchaseController.kt
@@ -1,16 +1,21 @@
 package com.bookmarket.controller
 
+import com.bookmarket.controller.mapper.PurchaseMapper
 import com.bookmarket.controller.request.PostPurchaseRequest
+import com.bookmarket.service.PurchaseService
 import org.springframework.http.HttpStatus.CREATED
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/purchases")
-class PurchaseController {
+@RequestMapping("purchases")
+class PurchaseController(
+    private val purchaseService: PurchaseService,
+    private val purchaseMapper: PurchaseMapper
+) {
 
     @PostMapping
     @ResponseStatus(CREATED)
     fun create(@RequestBody request: PostPurchaseRequest) {
-
+        purchaseService.create(purchaseMapper.toModel(request))
     }
 }
--- src/main/kotlin/com/bookmarket/controller/request/PostPurchaseRequest.kt
+++ src/main/kotlin/com/bookmarket/controller/request/PostPurchaseRequest.kt
@@ -1,12 +1,15 @@
 package com.bookmarket.controller.request
 
+import com.fasterxml.jackson.annotation.JsonAlias
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Positive
 
 data class PostPurchaseRequest(
     @field:NotNull
     @field:Positive
+    @JsonAlias("customer_id")
     val customerId: Int,
     @field:NotNull
+    @JsonAlias("book_ids")
     val bookIds: Set<Int>
 )
